ad14b4f1da1b8c33fc2ffbc9cd0a4700d53dd39c
Jaehun <jaehun1417@gmail.com>
2025-09-12 14:43:32 +0900

    곱이여#1

---
+++ src/multi.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class multi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.printf("이것은 제곱이여 :%d", n*n);
+    }
+}
+
+
